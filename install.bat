19be233f07b862cb4fd3ecd8d76bed7320e18288
Maxim Golov <maxim.golov@gmail.com>
2013-05-20 23:15:44 +0200

    Create install folder if it does not exist

--- install.bat
+++ install.bat
@@ -2,6 +2,13 @@
 
 set DEST=d:\bin\phototools
 
+if not exist %DEST% md %DEST%
+if not exist %DEST% (
+	echo ERROR: cannot create %DEST%
+	pause
+	exit /b
+)
+
 attrib -r %DEST%\import*.bat
 
 copy /y import*.bat %DEST%
