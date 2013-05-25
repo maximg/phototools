b8d7af293b7ba0861dd962a66ec2d52c7174505d
Maxim Golov <maxim.golov@gmail.com>
2013-05-25 20:42:19 +0200

    Install to C:\bin if one is present

--- install.bat
+++ install.bat
@@ -1,6 +1,14 @@
 ::  Deploy all scripts to default installation location
 
-set DEST=d:\bin\phototools
+set DEST=
+dir c: && if exist c:\bin set DEST=c:\bin\phototools
+dir d: && if exist d:\bin set DEST=d:\bin\phototools
+
+if '%DEST%' == '' (
+	echo ERROR: cannot find the installation location
+	pause
+	exit /b
+)
 
 if not exist %DEST% md %DEST%
 if not exist %DEST% (
