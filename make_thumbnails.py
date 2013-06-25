e091f21014b38d923c350b714e82574efb4c829c
Maxim Golov <maxim.golov@gmail.com>
2013-06-25 23:08:10 +0200

    Bit more work on video thumbnails

--- make_thumbnails.py
+++ make_thumbnails.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import datetime
+import time
 from collections import defaultdict
 import shutil
 
 def is_video(aFile):
-    extension = os.path.splitext(filename)[1]
-    return extension.toLower == ".mp4"  # todo
+    extension = os.path.splitext(aFile)[1]
+    return extension.lower() == ".mp4"  # todo
 
 def make_thumbnails(aFile, destdir):
     print "Generating thumbnails for %s" % (aFile)
@@ -18,11 +19,35 @@ def make_all_thumbnails(sourcedir, destdir):
                 fullpath = os.path.join(root, aFile)
                 make_thumbnails(fullpath, destdir)
                 #print "%s has file %s, mod date %s" % (root, ff, mod_date)
-	
+
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    if not os.access(path, os.W_OK):
+        # Is the error an access error ?
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
+                    
 if __name__ == "__main__":
-    root = "c:\\Users\\golov\\Documents\\GitHub\\phototools\\test\\make_thumbnails"
+    rootroot = "c:\\Users\\golov\\Documents"
+    #rootroot = "d:\\Projects"
+    root = rootroot + "\\GitHub\\phototools\\test\\make_thumbnails"
     workdir = os.path.join(root, "workdir")
-    shutil.rmtree(workdir)
+    if os.path.exists(workdir):
+        shutil.rmtree(workdir, onerror=onerror)
+    time.sleep(0.2) # race condition with removing the tree above
+    os.makedirs(workdir)
     for dir in ("source", "dest"):
         shutil.copytree(os.path.join(root, dir), os.path.join(workdir, dir))
 
@@ -30,6 +55,6 @@ if __name__ == "__main__":
         os.path.join(workdir, "source"),
         os.path.join(workdir, "dest"))
 
-	if False:
-		if not compare_tree( workdir, os.path.join(root, "expected")):
-			print "ERROR: test failed"
+    if False:
+        if not compare_tree( workdir, os.path.join(root, "expected")):
+            print "ERROR: test failed"
